8495371a53e30316a9996621ab7d2fc4d85465a7
reinmeia21 <mejiareignforworkplace@gmail.com>
2026-02-23 11:46:58 +0800

    js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+gsap.to(".box", {
+    rotate: 360,
+    duration: 2,
+    repeat: -1
+});
+
+MouseEvent.on("move", (event) => {
+    gsap.to(".box", {
+        x: event.clientX,
+        y: event.clientY,
+        duration: 0.5,
+        ease: "power2.out"
+    });
+});
+
+
